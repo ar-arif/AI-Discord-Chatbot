375aa9ae3183159328d5fcfee882838ce41ac650
Md. Ariful Islam <>
2021-05-01 15:45:11 +0000

    first deploy

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+import discord
+from discord.ext import commands
 import os
+from keep_alive import keep_alive
+from prsaw import RandomStuff
+
+rs = RandomStuff(async_mode=True)
+
 token = os.getenv('token')
 
+client = commands.Bot(command_prefix='>')
+
+@client.event
+async def on_ready():
+    os.system('clear')
+    print(f'{client.user.name} is ready to go.')
+
+
+@client.event
+async def on_message(message):
+  if client.user == message.author:
+    return
+  user_input = message.content
+  response = await rs.get_ai_response(user_input)
+  await message.reply(response)
+  await client.process_commands(message)
+  print(user_input)
+
+@client.command()
+async def ping(ctx):
+    lat = str(client.latency)[3] + str(client.latency)[4]
+    await ctx.send(f'pong {lat}ms')
+
+keep_alive()
+client.run(token)
